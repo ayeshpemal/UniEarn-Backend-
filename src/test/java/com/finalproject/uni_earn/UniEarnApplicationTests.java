c8072502019f2b2841db402256e8b4c5528d1f4f
ayeshapemal <ayeshapemal@gmail.com>
2024-12-25 18:34:50 +0530

    Backend initialized

---
+++ src/test/java/com/finalproject/uni_earn/UniEarnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.uni_earn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniEarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
