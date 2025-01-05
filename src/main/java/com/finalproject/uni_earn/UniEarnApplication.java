018a2355c204dd6ca29e71687b6b5a2e668913ab
Ayesha pemal <68103074+ayeshpemal@users.noreply.github.com>
2025-01-05 11:39:03 +0530

    Update UniEarnApplication.java

--- src/main/java/com/finalproject/uni_earn/UniEarnApplication.java
+++ src/main/java/com/finalproject/uni_earn/UniEarnApplication.java
@@ -1,6 +1,5 @@
 package com.finalproject.uni_earn;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +9,5 @@ public class UniEarnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UniEarnApplication.class, args);
 	}
+
 }
