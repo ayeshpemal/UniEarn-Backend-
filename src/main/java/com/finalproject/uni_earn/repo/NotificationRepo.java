440190ea745060e4ce17ad0dcb7f72fac7139977
Hasinduni <umeshahasinduni9@gmail.com>
2025-02-10 12:57:22 +0530

    JobNotification created

--- src/main/java/com/finalproject/uni_earn/repo/NotificationRepo.java
+++ src/main/java/com/finalproject/uni_earn/repo/NotificationRepo.java
@@ -1,12 +1,12 @@
 package com.finalproject.uni_earn.repo;
 
-import com.finalproject.uni_earn.entity.Notification;
+import com.finalproject.uni_earn.entity.JobNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @EnableJpaRepositories
-public interface NotificationRepo extends JpaRepository<Notification, Long> {
+public interface NotificationRepo extends JpaRepository<JobNotification, Long> {
 
 }
