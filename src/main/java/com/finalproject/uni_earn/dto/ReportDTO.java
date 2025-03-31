0b1b6cb3a614426c01f9a7489e650fa3b5d939ad
Ayesha pemal <ayeshapemal@gmail.com>
2025-03-31 22:58:06 +0530

    fixed issues in report functions

--- src/main/java/com/finalproject/uni_earn/dto/ReportDTO.java
+++ src/main/java/com/finalproject/uni_earn/dto/ReportDTO.java
@@ -3,13 +3,19 @@ package com.finalproject.uni_earn.dto;
 import com.finalproject.uni_earn.entity.User;
 import com.finalproject.uni_earn.entity.enums.ReportState;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ReportDTO {
 //    this is normal reportDTO uses in report service ,specially when you need all the details of the report, paged report entity give to data this type
     private Long reportId;
-    private User reporter;
-    private User reportedUser;
+    private Long reporter;
+    private Long reportedUser;
     private String feedback;
     private LocalDateTime reportDate;
     private ReportState status;
--- src/main/java/com/finalproject/uni_earn/service/ReportProcessService.java
+++ src/main/java/com/finalproject/uni_earn/service/ReportProcessService.java
@@ -5,4 +5,6 @@ import com.finalproject.uni_earn.entity.enums.ReportType;
 public interface ReportProcessService {
 
     public Double getScore(ReportType reportType);
+
+    boolean isValidReportType(ReportType reportType);
 }
